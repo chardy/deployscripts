1a7ab68bb2cbb26be6398d065ff858e55370ac04
chardy@gmail.com <chardy@gmail.com>
2013-12-20 14:12:07 +0800

    fixing nodejs installation

--- lib/nodejs.sh
+++ lib/nodejs.sh
@@ -3,8 +3,8 @@
 function install_nodejs {
   log "install_nodejs: Installing NodeJS..."
   apt-get -y install software-properties-common python-software-properties python g++ make
-  add-apt-repository ppa:chris-lea/node.js
+  add-apt-repository -y ppa:chris-lea/node.js
   apt-get -y update
-  apt-get -y install nodejs nodejs-dev
+  apt-get -y install nodejs
   npm install -g uglify-js@1
 }
