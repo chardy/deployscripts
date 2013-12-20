02e5151a05db79a829ec753eed5662dd56acf10b
chardy@gmail.com <chardy@gmail.com>
2013-12-20 12:58:03 +0800

    add basic install

--- lib/utils.sh
+++ lib/utils.sh
@@ -9,7 +9,7 @@ function log {
     log "log: requires text string as its argument"
     return 1;
   fi
-  echo "`date '+%D %T'` -  $1" >> $LOG_FILE
+  # echo "`date '+%D %T'` -  $1" >> $LOG_FILE
   echo "`date '+%D %T'` -  $1"
 }
 
@@ -24,19 +24,20 @@ function ask {
 
 function upgrade_system {
   log "upgrade_system: Upgrading System..."
-  aptitude update
+  apt-get update
   aptitude -y install aptitude
   aptitude -y full-upgrade
+
 }
 
 function make_sure_no_apache {
   log "make_sure_no_apache: Need to make sure apache is not running, some Cloud provider install apache by default..."
   apt-get -y purge apache2 apache2-utils apache2.2-bin apache2-common
   apt-get autoremove --purge
-  
+
 }
 
-function install_essentials {
+function install_basics {
   log "install_essentials: Installing Essentials..."
   apt-get -y install autoconf automake bash-completion bison build-essential curl dnsutils freetds-bin freetds-dev git-core less libc6-dev libcurl3 libcurl3-gnutls libcurl4-openssl-dev libffi-devlibpcre3-dev libreadline-dev libreadline5-dev libreadline6 libreadline6-dev libsqlite3-0 libsqlite3-dev libssl-dev libsvn-dev libtool libxml2 libxml2-dev libxslt-dev libxslt1-dev libyaml-dev locate ncurses-dev openssh-server openssl rsync sqlite-doc sqlite3 subversion subversion-tools sudo tdsodbc unzip vim wget whois zlib1g zlib1g-dev
 
